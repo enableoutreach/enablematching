d307270b248d5c5fb2a1b7e9dab1d584a5cce362
Joe Cross <joe@enableoutreach.org>
2017-03-20 10:56:38 +0100

    Commit it all

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,4 +1,7 @@
 Rails.application.configure do
+  
+    ENV['MAIL_PASSWORD'] = '***REMOVED***'
+  
   # Settings specified here will take precedence over those in config/application.rb.
 config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 
